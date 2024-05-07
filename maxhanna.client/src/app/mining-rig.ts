8cba518418a116e542409a0dc9b306877277e54d
Max Hanna <saintminion@hotmail.com>
2024-05-07 14:43:10 -0400

    added mining rig devices to mining rig class

--- maxhanna.client/src/app/mining-rig.ts
+++ maxhanna.client/src/app/mining-rig.ts
@@ -1,3 +1,5 @@
+import { MiningRigDevice } from "./mining-rig-device";
+
 export class MiningRig {
   rigId: string | undefined;
   rigName: string | undefined; 
@@ -6,4 +8,5 @@ export class MiningRig {
   speedRejected: number | undefined;
   localProfitability: number | undefined;
   actualProfitability: number | undefined;
+  devices: Array<MiningRigDevice> | undefined;
 }
