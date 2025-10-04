db3252ba9b151130c3edf0d5bde47b80b6f60263
maxha_cae <maxha@cae.com>
2025-10-04 16:48:40 -0400

    app menu item

---
+++ maxhanna.client/src/app/app-menu-item/app-menu-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app-menu-item.component.html',
+  styleUrls: ['./app-menu-item.component.css']
+})
+export class AppMenuItemComponent {
+  @Input() type: string = '';
+  @Input() parentRef: any;
+  @Input() label?: string;
+  @Input() className?: string;
+
+  open() {
+    if (this.parentRef && typeof this.parentRef.createComponent === 'function') {
+      this.parentRef.createComponent(this.type);
+    }
+  }
+
+  get displayLabel() {
+    return this.label ?? this.type;
+  }
+}
