4144fb5daecc92f6092526dac848ef4e0fe729cc
maxha_cae <maxha@cae.com>
2025-10-13 18:44:07 -0400

    -8 offset

--- maxhanna.client/src/app/ender/objects/Environment/bike-wall.ts
+++ maxhanna.client/src/app/ender/objects/Environment/bike-wall.ts
@@ -32,7 +32,7 @@ export class BikeWall extends GameObject {
       drawLayer: FLOOR,
       name: "bike-wall",
       offsetY: -16,
-      offsetX: -16,
+      offsetX: -8,
       colorSwap: params.colorSwap
     });
     this.addChild(body);
