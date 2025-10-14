b8af3a6fd6b678ed282d7312c53a87440318d14a
maxha_cae <maxha@cae.com>
2025-10-14 12:37:54 -0400

    refac hero speed

--- maxhanna.client/src/app/ender/objects/Hero/hero.ts
+++ maxhanna.client/src/app/ender/objects/Hero/hero.ts
@@ -61,7 +61,13 @@ export class Hero extends Character {
     this.facingDirection = DOWN;
     this.destinationPosition = this.position.duplicate();
     this.lastPosition = this.position.duplicate();
-    this.speed = (params.isUserControlled ? (params.speed ?? 1) : ((params.speed ?? 1) > 1 ? params.speed : 4));
+    // Ensure speed is always a number. User-controlled heroes default to 1.
+    // Non-user bots prefer a higher speed if provided (>1), otherwise default to 4.
+    if (params.isUserControlled) {
+      this.speed = params.speed ?? 1;
+    } else {
+      this.speed = (params.speed !== undefined && params.speed > 1) ? params.speed : 4;
+    }
     this.mask = params.mask;
     this.itemPickupTime = 0;
     this.isOmittable = false;
