f78ed6256ca9297b471c13bc19d90a179e6d8c9e
maxha_cae <maxha@cae.com>
2025-10-14 19:59:04 -0400

    remove useless class

--- maxhanna.Server/Controllers/DataContracts/Ender/UpdateBotPartsRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace maxhanna.Server.Controllers.DataContracts.Ender
-{
-    public class UpdateBotPartsRequest
-    {
-        public int HeroId { get; set; }
-        public MetaBotPart[]? Parts { get; set; }
-    }
-}
