a0ac2ea4a0f85dd0a9ce269a70f9de6942bf58c0
maxha_cae <maxha@cae.com>
2025-10-19 18:26:59 -0400

    loop

--- maxhanna.client/src/app/bones/helpers/game-loop.ts
+++ maxhanna.client/src/app/bones/helpers/game-loop.ts
@@ -1,7 +1,52 @@
 export class GameLoop {
-  lastFrameTime = 0; accumulatedTime = 0; timeStep = 1000/60; update: Function; render: Function; rafId?: number; isRunning = false;
-  constructor(update: Function, render: Function) { this.update = update; this.render = render; }
-  mainLoop = async (timestamp: number) => { if (!this.isRunning) return; let deltaTime = timestamp - this.lastFrameTime; this.lastFrameTime = timestamp; this.accumulatedTime += deltaTime; while (this.accumulatedTime >= this.timeStep) { await this.update(this.timeStep); this.accumulatedTime -= this.timeStep; } this.render(); this.rafId = requestAnimationFrame(this.mainLoop); }
-  start() { if (!this.isRunning) { this.isRunning = true; this.rafId = requestAnimationFrame(this.mainLoop); } }
-  stop() { if (this.rafId) cancelAnimationFrame(this.rafId); this.isRunning = false; }
+  lastFrameTime = 0;
+  accumulatedTime = 0;
+  timeStep = 1000 / 60;
+  update: Function;
+  render: Function;
+  ctx?: CanvasRenderingContext2D = undefined;
+  rafId?: number;
+  isRunning = false;
+
+  constructor(update: Function, render: Function) {
+    this.update = update;
+    this.render = render; 
+  }
+
+  getCtx() {
+    const canvas: HTMLCanvasElement = document.getElementById("gameCanvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+    return ctx;
+  }
+  mainLoop = async (timestamp: number) => {
+    //console.log(timestamp);
+    if (!this.isRunning) return;
+
+    let deltaTime = timestamp - this.lastFrameTime;
+    this.lastFrameTime = timestamp;
+    this.accumulatedTime += deltaTime;
+
+    while (this.accumulatedTime >= this.timeStep) { 
+      await this.update(this.timeStep);
+      this.accumulatedTime -= this.timeStep;
+    }
+
+    this.render();
+
+    this.rafId = requestAnimationFrame(this.mainLoop);
+  }
+
+
+  start() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.rafId = requestAnimationFrame(this.mainLoop);
+    }
+  }
+  stop() {
+    if (this.rafId) {
+      cancelAnimationFrame(this.rafId);
+    }
+    this.isRunning = false;
+  }
 }
