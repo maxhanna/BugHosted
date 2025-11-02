2716c5db5a926f5ffb3fca01f22d19e81822ac50
maxha_cae <maxha@cae.com>
2025-11-01 20:32:19 -0400

    fx for mod

--- maxhanna.client/src/app/app.module.ts
+++ maxhanna.client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {
@@ -186,6 +187,7 @@ import { CurrentlyPlayingComponent } from './currently-playing/currently-playing
   ],
   bootstrap: [AppComponent],
   imports: [BrowserModule,
+    CommonModule,
   AppMenuItemComponent,
   LineGraphComponent,
   TimeFormatPipe,
