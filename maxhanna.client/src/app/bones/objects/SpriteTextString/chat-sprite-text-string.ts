978c9b96bf0dc3b66540b1463010b3852760bac1
maxha_cae <maxha@cae.com>
2025-11-04 14:05:31 -0500

    offset 0

--- maxhanna.client/src/app/bones/objects/SpriteTextString/chat-sprite-text-string.ts
+++ maxhanna.client/src/app/bones/objects/SpriteTextString/chat-sprite-text-string.ts
@@ -15,7 +15,7 @@ export class ChatSpriteTextString extends GameObject {
   // Increased from 14 to 16 for more readable chat line spacing
   LINE_VERTICAL_WIDTH = 16;
   TIME_UNTIL_DESTROY = 8000;
-  private readonly chatWindowOffset = new Vector2(20, 40);
+  private readonly chatWindowOffset = new Vector2(0, 40);
   objectSubject: any;
   content: string[] = [];
   // Cache for words
@@ -36,7 +36,7 @@ export class ChatSpriteTextString extends GameObject {
     objectSubject?: any;
   }) {
     super({
-      position: new Vector2(config.objectSubject.position.x + 20, config.objectSubject.position.y + 40),
+      position: new Vector2(config.objectSubject.position.x + 0, config.objectSubject.position.y + 40),
       drawLayer: HUD, // Ensured high-priority layer
       name: "CHATSPRITETEXTSTRING"
     });
