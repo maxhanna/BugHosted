e88cc6a56e73451bb7987d00e44298f849d77729
maxha_cae <maxha@cae.com>
2025-11-04 18:43:23 -0500

    no overlap on portal

--- maxhanna.client/src/app/bones/objects/Environment/TownPortal/town-portal.ts
+++ maxhanna.client/src/app/bones/objects/Environment/TownPortal/town-portal.ts
@@ -47,7 +47,7 @@ export class TownPortal extends GameObject {
     });
     this.addChild(shadow);
     body?.animations?.play("portalAnimation");
-    resources.playSound('portalNoise', { volume: 1.0, allowOverlap: true });
+    resources.playSound('portalNoise', { volume: 1.0, allowOverlap: false });
   }
 
   override ready() {  
