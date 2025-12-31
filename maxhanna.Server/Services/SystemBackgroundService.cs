d6e453656a4427a9ea45a3a5891be5af8fb71ec5
Max Hanna <saintminion@hotmail.com>
2025-12-31 09:07:38 -0500

    RunThreeHourTasks

--- maxhanna.Server/Services/SystemBackgroundService.cs
+++ maxhanna.Server/Services/SystemBackgroundService.cs
@@ -120,7 +120,7 @@ namespace maxhanna.Server.Services
 		private async Task RunSmokeTests()
 		{
 			Console.WriteLine("Running initial smoke tests...");
-			await RunDailyTasks();
+			await RunThreeHourTasks();
 		}
 		private async Task Run10SecondTasks()
 		{
