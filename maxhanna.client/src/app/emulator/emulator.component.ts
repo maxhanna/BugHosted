f1a9a5a558087afbd5b3a8bbb83726f56c833d50
maxhanna <maxahanna@hotmail.com>
2026-02-22 16:29:57 -0500

    debug save

--- maxhanna.client/src/app/emulator/emulator.component.ts
+++ maxhanna.client/src/app/emulator/emulator.component.ts
@@ -1487,7 +1487,8 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
           const arr = new Uint8Array(await blob.arrayBuffer());
           const res = await this.romService.saveEmulatorJSState(rec.romName, rec.userId, arr);
           if (res.ok) {
-            await this.removePendingSave(rec.id);
+            //await this.removePendingSave(rec.id);
+            console.log("returned data from saveEmulatorJSState: ", res);
             console.log('[EJS] uploaded pending save for', rec.romName);
           } else {
             console.warn('[EJS] failed to upload pending save:', res.errorText);
