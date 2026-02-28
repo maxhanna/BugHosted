3178c0f1fe7b529b1842a36c23f3d3360e913d26
Max Hanna <saintminion@hotmail.com>
2026-02-28 16:24:47 -0500

    switch 1 3 24

--- maxhanna.client/src/app/emulator/emulator.component.ts
+++ maxhanna.client/src/app/emulator/emulator.component.ts
@@ -725,16 +725,16 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
     } as Record<number, unknown>;
     w.EJS_defaultControls = {
       0: {
-        0: { value: 'x', value2: 'BUTTON_2' },
-        1: { value: 's', value2: 'BUTTON_4' },
+        0: { value: 'x', value2: 'BUTTON_1' },
+        1: { value: 's', value2: 'BUTTON_3' },
         2: { value: 'v', value2: 'SELECT' },
         3: { value: 'enter', value2: 'START' },
         4: { value: 'up arrow', value2: 'DPAD_UP' },
         5: { value: 'down arrow', value2: 'DPAD_DOWN' },
         6: { value: 'left arrow', value2: 'DPAD_LEFT' },
         7: { value: 'right arrow', value2: 'DPAD_RIGHT' },
-        8: { value: 'z', value2: 'BUTTON_1' },
-        9: { value: 'a', value2: 'BUTTON_3' },
+        8: { value: 'z', value2: 'BUTTON_2' },
+        9: { value: 'a', value2: 'BUTTON_4' },
         10: { value: 'q', value2: 'LEFT_TOP_SHOULDER' },
         11: { value: 'e', value2: 'RIGHT_TOP_SHOULDER' },
         12: { value: 'tab', value2: 'LEFT_BOTTOM_SHOULDER' },
