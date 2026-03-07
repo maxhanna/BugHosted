7379ae5f48aaa32da4a4382f79832d6328a0da95
Max Hanna <saintminion@hotmail.com>
2026-03-06 23:15:56 -0500

    rommetadata fixes

--- maxhanna.Server/Controllers/DataContracts/Files/RomMetadata.cs
+++ maxhanna.Server/Controllers/DataContracts/Files/RomMetadata.cs
@@ -5,21 +5,14 @@ namespace maxhanna.Server.Controllers.DataContracts.Files
   {
     public int? IgdbGameId { get; set; }
     public string? IgdbName { get; set; }
-    public string? Summary { get; set; }
-
-    /// <summary>
-    /// Keep this as long? if your DB stores IGDB first_release_date as unix seconds.
-    /// If you store DATETIME instead, change to DateTime?.
-    /// </summary>
+    public string? Summary { get; set; } 
     public long? FirstReleaseDateUnix { get; set; }
     public DateTime? FirstReleaseDate {get; set; } // This is for convenience so you don't have to convert from unix seconds on the client.
 
     public double? TotalRating { get; set; }
     public int? TotalRatingCount { get; set; }
 
-    public string? CoverUrl { get; set; }
-
-    // Keep as string because the DB column is JSON or text and you may send raw JSON down.
+    public string? CoverUrl { get; set; } 
     public string? ScreenshotsJson { get; set; }
     public string? ArtworksJson { get; set; }
     public string? VideosJson { get; set; }
