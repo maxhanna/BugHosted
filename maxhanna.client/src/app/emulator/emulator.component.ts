e6fa9237b6c0e972afde72317db0f3930262c63d
Max Hanna <saintminion@hotmail.com>
2026-03-13 16:53:45 -0400

    netplay enabled

--- maxhanna.client/src/app/emulator/emulator.component.ts
+++ maxhanna.client/src/app/emulator/emulator.component.ts
@@ -464,6 +464,7 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
     window.EJS_gameUrl = this.romObjectUrl;
     window.EJS_gameID = `${core}:${this.fileService.getFileWithoutExtension(fileName)}`;
     window.EJS_gameName = this.fileService.getFileWithoutExtension(this.romName ?? '');
+    window.EJS_netplayServer = 'http://bughosted.com:3000';
     window.EJS_startOnLoaded = true;
     window.EJS_volume = 0.5;
     window.EJS_lightgun = false;
@@ -3092,6 +3093,7 @@ declare global {
     EJS_paths?: { [key: string]: string };
     EJS_volume?: number;
     EJS_threads?: boolean;
+    EJS_netplayServer?: string;
     EJS_maxThreads?: number;
     EJS_color?: string;
     EJS_backgroundColor?: string;
