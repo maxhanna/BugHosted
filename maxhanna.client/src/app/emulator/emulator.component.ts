a4a8da5a2ce00cf7a2b434ee275909f0611b5d25
Max Hanna <saintminion@hotmail.com>
2026-03-17 11:57:36 -0400

    rem debug stuff

--- maxhanna.client/src/app/emulator/emulator.component.ts
+++ maxhanna.client/src/app/emulator/emulator.component.ts
@@ -380,9 +380,7 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
     // If the build calls back with the instance, capture it early
 
     window.EJS_ready = (api: any) => {
-      try { 
-        (window as any).EJS_DEBUG_XX = true; 
-        (window as any).EJS_EXPERIMENTAL_NETPLAY = true;  
+      try {
         this._ejsReady = true;
         this.scanAndTagVpadControls();
         this.emulatorInstance = api || window.EJS || window.EJS_emulator || this.emulatorInstance;
@@ -801,8 +799,8 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
     if (system === "saturn" || core === "yabause") {
 
     }
-    w.EJS_DEBUG_XX = false;            // MUST be false here so loader.js loads emulator.min.js
-    w.EJS_EXPERIMENTAL_NETPLAY = false; // will be enabled after loader.js finishes (in EJS_ready)
+    w.EJS_DEBUG_XX = true;             // debug options 
+    w.EJS_EXPERIMENTAL_NETPLAY = true; // required alongside EJS_DEBUG_XX for netplay
     w.EJS_logCoreInfo = false;          // debug options 
     w.EJS_logVideo = false;             // debug options 
     w.EJS_logAudio = false;             // debug options 
