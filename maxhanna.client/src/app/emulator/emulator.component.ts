488f2ef2a9d33fd3efdf0ecb0870b51015589505
maxhanna <maxahanna@hotmail.com>
2026-03-29 12:37:45 -0400

    debug

--- maxhanna.client/src/app/emulator/emulator.component.ts
+++ maxhanna.client/src/app/emulator/emulator.component.ts
@@ -381,7 +381,7 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
 
     window.EJS_ready = (api: any) => {
       try {
-        (window as any).EJS_DEBUG_XX = false;
+        (window as any).EJS_DEBUG_XX = true;
         (window as any).EJS_EXPERIMENTAL_NETPLAY = true;
         this._ejsReady = true;
         this.scanAndTagVpadControls();
@@ -861,7 +861,7 @@ export class EmulatorComponent extends ChildComponent implements OnInit, OnDestr
       2: { ...gpOnly },
       3: { ...gpOnly },
     };
-    w.EJS_logCoreInfo = false;          // debug options 
+    w.EJS_logCoreInfo = true;          // debug options 
     w.EJS_logVideo = false;             // debug options 
     w.EJS_logAudio = false;             // debug options 
     w.EJS_logInput = false;             // debug options 
