5f6ad10f96364bdea3736082cf9f2b735951103c
maxhanna <maxahanna@hotmail.com>
2026-05-08 16:42:48 -0400

    fx globe

--- maxhanna.client/src/app/globe/globe.component.ts
+++ maxhanna.client/src/app/globe/globe.component.ts
@@ -197,21 +197,18 @@ export class GlobeComponent implements OnInit {
     const height = 2;
     const fallbackData = new Uint8Array([0, 50, 150, 255, 0, 50, 150, 255, 0, 50, 150, 255, 0, 50, 150, 255]);
     this.gl!.texImage2D(this.gl!.TEXTURE_2D, 0, this.gl!.RGBA, width, height, 0, this.gl!.RGBA, this.gl!.UNSIGNED_BYTE, fallbackData);
-    this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.LINEAR_MIPMAP_LINEAR, this.gl!.LINEAR);
+    this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_MIN_FILTER, this.gl!.LINEAR);
     this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_MAG_FILTER, this.gl!.LINEAR);
-    this.gl!.generateMipmap(this.gl!.TEXTURE_2D);
 
-    // Try to load real Earth texture from Wikipedia/NASA
+    // Try to load real Earth texture
     const img = new Image();
-    img.crossOrigin = 'anonymous';
     img.onload = () => {
       this.gl!.bindTexture(this.gl!.TEXTURE_2D, this.texture);
       this.gl!.texImage2D(this.gl!.TEXTURE_2D, 0, this.gl!.RGBA, this.gl!.RGBA, this.gl!.UNSIGNED_BYTE, img);
-      this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_MIN_FILTER, this.gl!.LINEAR_MIPMAP_LINEAR);
+      this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_MIN_FILTER, this.gl!.LINEAR);
       this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_MAG_FILTER, this.gl!.LINEAR);
-      this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_WRAP_S, this.gl!.REPEAT);
+      this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_WRAP_S, this.gl!.CLAMP_TO_EDGE);
       this.gl!.texParameteri(this.gl!.TEXTURE_2D, this.gl!.TEXTURE_WRAP_T, this.gl!.CLAMP_TO_EDGE);
-      this.gl!.generateMipmap(this.gl!.TEXTURE_2D);
     };
     img.onerror = () => {
       // Fallback: generate a better procedural texture
