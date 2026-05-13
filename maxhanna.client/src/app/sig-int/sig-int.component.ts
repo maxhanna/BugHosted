fbb0b55b4f5e8def364b72c1db9d994f558c8acc
maxhanna <maxahanna@hotmail.com>
2026-05-13 16:36:56 -0400

    sigint fx

--- maxhanna.client/src/app/sig-int/sig-int.component.ts
+++ maxhanna.client/src/app/sig-int/sig-int.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ChildComponent } from '../child.component';
-import { NewsService } from '../../services/news.service';
-import { SocialService } from '../../services/social.service';
-import { Article } from '../../services/datacontracts/news/news-data';
-import { Story } from '../../services/datacontracts/social/story';
-import { NewsPin } from '../../services/datacontracts/news/news-data';
 
 @Component({
   selector: 'app-sig-int',
@@ -13,78 +8,13 @@ import { NewsPin } from '../../services/datacontracts/news/news-data';
   styleUrl: './sig-int.component.css'
 })
 export class SigIntComponent extends ChildComponent implements OnInit {
-  isMenuPanelOpen = false;
-
-  stories: Story[] = [];
-  newsPins: NewsPin[] = [];
-  newsArticles: Article[] = [];
-  selectedCard: { type: 'story' | 'newsPin' | 'article', item: Story | NewsPin | Article } | null = null;
-  isLoadingCards = false;
-
-  constructor(
-    private readonly newsService: NewsService,
-    private readonly socialService: SocialService,
-  ) {
+  constructor() {
     super();
   }
 
-  async ngOnInit() {
-    await this.loadCards();
-  }
+  isMenuPanelOpen = false;
 
-  async loadCards() {
-    this.isLoadingCards = true;
-    await this.loadStories();
-    await this.loadNewsPins();
-    await this.loadNewsArticles();
-    this.isLoadingCards = false;
-  }
-
-  async loadStories() {
-    const storiesData = await this.socialService.getStories(undefined, undefined, undefined, undefined, undefined, 1, 50);
-    if (storiesData?.stories) {
-      this.stories = storiesData.stories;
-    }
-  }
-
-  async loadNewsPins() {
-    this.newsPins = await this.newsService.getNewsPins();
-  }
-
-  async loadNewsArticles() {
-    const newsData = await this.newsService.getAllNews(1, 30);
-    if (newsData?.articles) {
-      this.newsArticles = newsData.articles;
-    }
-  }
-
-  onCardSelect(type: 'story' | 'newsPin' | 'article', item: Story | NewsPin | Article) {
-    this.selectedCard = { type, item };
-  }
-
-  getStoryAuthor(story: Story): string {
-    return story.user?.username ?? 'Anonymous';
-  }
-
-  getStoryTimeAgo(story: Story): string {
-    return story.date ? story.date.toLocaleDateString() : '';
-  }
-
-  getPinLocation(pin: NewsPin): string {
-    const parts: string[] = [];
-    if (pin.label) parts.push(pin.label);
-    if (pin.locationType) parts.push(pin.locationType);
-    return parts.join(' - ') || 'Unknown Location';
-  }
-
-  formatDate(date: Date | null): string {
-    if (!date) return '';
-    return new Date(date).toLocaleDateString();
-  }
-
-  truncate(str: string, len: number): string {
-    if (!str) return '';
-    return str.length > len ? str.slice(0, len) + '...' : str;
+  ngOnInit(): void {
   }
   ngOnDestroy(): void {
     this.remove_me("SigIntComponent");
