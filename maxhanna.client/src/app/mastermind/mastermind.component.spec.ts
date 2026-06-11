e52f09b06404aac5949257015ad4c75b5b807d90
maxhanna <maxahanna@hotmail.com>
2026-06-11 11:33:58 -0400

    fx mastermind tests

--- maxhanna.client/src/app/mastermind/mastermind.component.spec.ts
+++ maxhanna.client/src/app/mastermind/mastermind.component.spec.ts
@@ -1,6 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MastermindComponent } from './mastermind.component';
+import { TitleBarComponent } from '../title-bar/title-bar.component';
+import { ShareButtonComponent } from '../share-button/share-button.component';
+import { MastermindScoresComponent } from '../mastermind-scores/mastermind-scores.component';
+
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    declarations: [
+      MastermindComponent,
+      TitleBarComponent,
+      ShareButtonComponent,
+      MastermindScoresComponent,
+    ]
+  }).compileComponents();
+});
 
 describe('MastermindComponent', () => {
   let component: MastermindComponent;
