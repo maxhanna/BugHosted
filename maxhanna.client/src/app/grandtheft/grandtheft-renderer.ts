c5bd0ff511859ba415ae909a8ee68bc90e741f6f
maxhanna <maxahanna@hotmail.com>
2026-06-19 08:02:18 -0400

    -1 fix

--- maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
+++ maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
@@ -1200,7 +1200,7 @@ void main() {
     if (meshList.some(m => m.needsFlip)) {
       mat4.rotateX(this.modelMatrix, this.modelMatrix, Math.PI);
       mat4.rotateY(this.modelMatrix, this.modelMatrix, Math.PI);
-      mat4.translate(this.modelMatrix, this.modelMatrix, [x, y - 10, z]);
+      mat4.translate(this.modelMatrix, this.modelMatrix, [x, y - 1, z]);
     }
 
     mat4.scale(this.modelMatrix, this.modelMatrix, scale);
