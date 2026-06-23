edd6a5aa82a8f8c5545a2f92b6cce789494bc0a3
maxhanna <maxahanna@hotmail.com>
2026-06-23 14:59:20 -0400

    bigger orb. move z

--- maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
+++ maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
@@ -2304,8 +2304,8 @@ void main() {
           for (let ci = 0; ci < corners.length; ci++) {
             const lx = node.x + corners[ci][0];
             const lz = node.z + corners[ci][1];
-            this.drawMesh(this.getSphereMesh(0.06), lx + 0.18, 3.85, lz + 0.05, 0, [1, 1, 1], redOn ? [1, 0.1, 0.1, 1] : [0.05, 0.15, 0.05, 0.4]);
-            this.drawMesh(this.getSphereMesh(0.06), lx + 0.18, 3.65, lz + 0.05, 0, [1, 1, 1], redOn ? [0.05, 0.15, 0.05, 0.4] : [0.1, 1, 0.1, 1]);
+            this.drawMesh(this.getSphereMesh(0.075), lx + 0.18, 3.85, lz + 0.065, 0, [1, 1, 1], redOn ? [1, 0.1, 0.1, 1] : [0.05, 0.15, 0.05, 0.4]);
+            this.drawMesh(this.getSphereMesh(0.075), lx + 0.18, 3.65, lz + 0.065, 0, [1, 1, 1], redOn ? [0.05, 0.15, 0.05, 0.4] : [0.1, 1, 0.1, 1]);
           }
         }
       } else {
