31451ed433ef7a2d3d999c5e144804420d0ec588
maxhanna <maxahanna@hotmail.com>
2026-06-23 07:22:07 -0400

    move arms back

--- maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
+++ maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
@@ -2741,7 +2741,7 @@ void main() {
     if (this.firstPersonArmsMesh) {
       const ax = camX + fx * 0.2 + rightX * 0.06;
       const ay = camY + fy * 0.2 - 0.5;
-      const az = camZ + fz * 3.2 + rightZ * 0.06;
+      const az = camZ + fz * 2.2 + rightZ * 0.06;
       this.drawMesh(this.firstPersonArmsMesh, ax, ay, az, camYaw, [0.6, 0.6, 0.6], [1, 1, 1, 1]);
     }
     if (weapon === 1 && this.mark23Mesh) {
