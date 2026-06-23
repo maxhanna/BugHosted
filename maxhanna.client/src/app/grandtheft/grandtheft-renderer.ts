e89d88e88afac0088b62ad5a390040444fbb079c
maxhanna <maxahanna@hotmail.com>
2026-06-23 06:31:57 -0400

    move gun forward, move arms back

--- maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
+++ maxhanna.client/src/app/grandtheft/grandtheft-renderer.ts
@@ -2738,15 +2738,15 @@ void main() {
     const fz = Math.cos(camYaw) * Math.cos(camPitch);
     const rightX = Math.cos(camYaw), rightZ = -Math.sin(camYaw);
     if (this.firstPersonArmsMesh) {
-      const ax = camX + fx * 0.18 + rightX * 0.06;
-      const ay = camY + fy * 0.18 - 2.5;
-      const az = camZ + fz * 0.18 + rightZ * 0.06;
+      const ax = camX + fx * 0.3 + rightX * 0.15;
+      const ay = camY + fy * 0.3 - 0.7;
+      const az = camZ + fz * 0.3 + rightZ * 0.15;
       this.drawMesh(this.firstPersonArmsMesh, ax, ay, az, camYaw, [0.6, 0.6, 0.6], [1, 1, 1, 1]);
     }
     if (weapon === 1 && this.mark23Mesh) {
-      const mx = camX + fx * 0.4 + rightX * 0.06;
+      const mx = camX + fx * 0.4 + rightX * 0.26;
       const my = camY + fy * 0.4 - 2.2;
-      const mz = camZ + fz * 0.4 + rightZ * 0.06;
+      const mz = camZ + fz * 0.4 + rightZ * 0.26;
       this.drawMesh(this.mark23Mesh, mx, my, mz, camYaw, [1, 1, 1], [1, 1, 1, 1]);
     }
     gl.enable(gl.DEPTH_TEST);
